7855ab98fba6b5848203a88124a224347e295a1a
databit <contacto@databit.cl>
2023-11-08 02:13:26 -0300

    Commit Modelo Usuario

--- app/src/main/java/com/databit/appsqlite/Modelo/Usuario.java
+++ app/src/main/java/com/databit/appsqlite/Modelo/Usuario.java
@@ -35,4 +35,14 @@ public class Usuario {
     public void setCorreo(String correo) {
         this.correo = correo;
     }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", correo='" + correo + '\'' +
+                '}';
+    }
+
 }
