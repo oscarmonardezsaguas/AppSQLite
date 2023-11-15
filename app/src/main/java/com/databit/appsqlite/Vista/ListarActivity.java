abdf95161b33a0f85d21966fa9b14466f6fb5a41
databit <contacto@databit.cl>
2023-11-15 16:24:39 -0300

    Commit sin lista y mantencion

--- app/src/main/java/com/databit/appsqlite/Vista/ListarActivity.java
+++ app/src/main/java/com/databit/appsqlite/Vista/ListarActivity.java
@@ -29,6 +29,58 @@ public class ListarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listar);
 
+        // Configuración del ListView
+        listViewUsuarios=(ListView) findViewById(R.id.listViewUsuarios);
+
+        // Inicialización de la conexión a la base de datos
+        conn=new ConexionHelper(getApplicationContext(), "bd_usuarios", null, 1 );
+
+
+        consultarListaUsuarios();
+
+        // Creación de un ArrayAdapter para mostrar la lista en el ListView
+        ArrayAdapter adaptador=new ArrayAdapter(this, android.R.layout.simple_list_item_1,
+                listaInformacion);
+        listViewUsuarios.setAdapter(adaptador);
+
+
+        // Configuración del evento de clic en un elemento de la lista
+        listViewUsuarios.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String informacion="id: " + listaUsuarios.get(position).getId() +"\n";
+                informacion+="Nombres: " + listaUsuarios.get(position).getNombre() +"\n";
+                informacion+="Correo: " + listaUsuarios.get(position).getCorreo() +"\n";
+                Toast.makeText(getApplicationContext(), informacion,Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    // realiza la consulta a la base de datos y construye la lista de usuarios.
+    private void consultarListaUsuarios() {
+        SQLiteDatabase db = conn.getReadableDatabase();
+        Usuario usuario=null;
+        listaUsuarios=new ArrayList<Usuario>();
+
+        //La consulta se realiza mediante un objeto Cursor que ejecuta la consulta SQL:
+        Cursor cursor=db.rawQuery("SELECT * FROM "+ Utility.TABLA_USUARIO, null);
+        while (cursor.moveToNext()){
+            usuario=new Usuario();
+            usuario.setId(cursor.getInt(0));
+            usuario.setNombre(cursor.getString(1));
+            usuario.setCorreo(cursor.getString(2));
+            listaUsuarios.add(usuario);
+        }
+        obtenerLista();
+    }
+
+    //  crea una lista de cadenas (listaInformacion) que se utilizará para llenar el ArrayAdapter.
+    private void obtenerLista(){
+        listaInformacion=new ArrayList<String>();
+        for (int i=0; i<listaUsuarios.size(); i++){
+            listaInformacion.add(listaUsuarios.get(i).getId()+ " - " + listaUsuarios.get(i).getNombre());
+        }
     }
 
 }
--- app/src/main/java/com/databit/appsqlite/Vista/MaintenanceActivity.java
+++ app/src/main/java/com/databit/appsqlite/Vista/MaintenanceActivity.java
@@ -20,7 +20,6 @@ public class MaintenanceActivity extends AppCompatActivity {
     EditText txtnombre;
     EditText txtcorreo;
     Button btnconsultar, btnupdate, btndelete;
-
     ConexionHelper conn;
 
     @Override
@@ -28,5 +27,112 @@ public class MaintenanceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maintenance);
 
+
+        // cadena de conexion
+        conn = new ConexionHelper(getApplicationContext(), "bd_usuarios", null, 1);
+
+        txtid = (EditText) findViewById(R.id.txtId);
+        txtnombre = (EditText) findViewById(R.id.txtNombre);
+        txtcorreo = (EditText) findViewById(R.id.txtCorreo);
+
+        btnconsultar = (Button) findViewById(R.id.btnSearch);
+        btnupdate = (Button) findViewById(R.id.btnUpdate);
+        btndelete = (Button) findViewById(R.id.btnDelete);
+
+        btnconsultar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                consultar();
+            }
+        });
+        btnupdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                actualizarUsuario();
+            }
+        });
+        btndelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                eliminarUsuario();
+            }
+        });
+    }
+
+    // metodo elimina registro
+    private void eliminarUsuario() {
+        SQLiteDatabase db = conn.getWritableDatabase();
+        String[] parametros = {txtid.getText().toString()};
+
+        db.delete(Utility.TABLA_USUARIO, Utility.CAMPO_ID + "=?", parametros);
+        Toast.makeText(getApplicationContext(), "ATENCION, se eliminó el usuario",
+                Toast.LENGTH_LONG).show();
+        txtid.setText("");
+        limpiar();
+        db.close();
+    }
+
+    // metodo actualiza registro
+    private void actualizarUsuario() {
+        SQLiteDatabase db = conn.getWritableDatabase();
+        String[] parametros = {txtid.getText().toString()};
+
+        ContentValues values = new ContentValues();
+        values.put(Utility.CAMPO_NOMBRE, txtnombre.getText().toString());
+        values.put(Utility.CAMPO_CORREO, txtcorreo.getText().toString());
+
+        db.update(Utility.TABLA_USUARIO, values, Utility.CAMPO_ID + "=?", parametros);
+        Toast.makeText(getApplicationContext(), "ATENCION, se actualizó el usuario",
+                Toast.LENGTH_LONG).show();
+       // limpiar();
+        txtnombre.setText("");
+        txtcorreo.setText("");
+        db.close();
+    }
+
+    // metodo del boton buscar
+    private void consultar() {
+        SQLiteDatabase db = conn.getReadableDatabase();
+        String[] parametros = {txtid.getText().toString()};
+
+        try {
+            Cursor cursor = db.rawQuery("SELECT " + Utility.CAMPO_NOMBRE + "," + Utility.CAMPO_CORREO +
+                    " FROM " + Utility.TABLA_USUARIO + " WHERE " + Utility.CAMPO_ID + "=? ", parametros);
+
+            cursor.moveToFirst();
+            txtnombre.setText(cursor.getString(0));
+            txtcorreo.setText(cursor.getString(1));
+
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "ATENCION, usuario no existe",
+                    Toast.LENGTH_LONG).show();
+            limpiar();
+        }
+    }
+
+    private void consultarSql() {
+        SQLiteDatabase db=conn.getReadableDatabase();
+        String[] parametros={txtid.getText().toString()};
+
+        try {
+            //select nombre,telefono from usuario where codigo=?
+            Cursor cursor=db.rawQuery("SELECT "+Utility.CAMPO_NOMBRE+","+Utility.CAMPO_CORREO+
+                    " FROM "+Utility.TABLA_USUARIO+" WHERE "+Utility.CAMPO_ID+"=? ",parametros);
+
+            cursor.moveToFirst();
+            txtnombre.setText(cursor.getString(0));
+            txtcorreo.setText(cursor.getString(1));
+
+        }catch (Exception e){
+            Toast.makeText(getApplicationContext(),"El documento no existe",Toast.LENGTH_LONG).show();
+            limpiar();
+        }
+    }
+
+
+    // metodo limpia los text
+    private void limpiar() {
+        txtnombre.setText("");
+        txtcorreo.setText("");
     }
 }
